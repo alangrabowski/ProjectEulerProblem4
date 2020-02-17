94ad808bd4cc7ef36a3ac5d7e738375cf624040e
Alan Grabowski <grabowskialan@gmail.com>
2020-02-17 01:23:24 +0100

    - correction of searched range (100-999 instead of 100-1000) - added comments

--- src/com/Problem4.java
+++ src/com/Problem4.java
@@ -7,18 +7,19 @@ public class Problem4 {
         StringBuilder sb = new StringBuilder(x);
         String reverseX = sb.reverse().toString();
         return x.equals(reverseX);
-    }
-    
+    }   //this method takes some number, converts it into String, reverses it and checks if the reversed String is the
+        //same as the first one
+
     public static void main(String[] args) {
         int x, max=0;
-        for (int i=100; i<=1000; i++){
-            for (int j=100; j<=1000; j++){
+        for (int i=100; i<=999; i++){
+            for (int j=100; j<=999; j++){
                 x = i*j;
                 if (isPalindrome(x)&&x>=max){
                 max=x;
                 }
             }
-        }
+        }//program checks every combination of multiplying two 3-digit numbers in search of highest palindrome.
         System.out.println(max);
     }
 }
